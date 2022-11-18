f63c721a5e0a6ae8aa6b890edd863b5a9fbdbe87
ryanc386 <98830588+ryanc386@users.noreply.github.com>
2022-11-17 21:37:32 -0500

    Update main.c

--- Q3/main.c
+++ Q3/main.c
@@ -2,17 +2,14 @@
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
-
-    P1DIR |= BIT6 | BIT7;                     // P1.6 and P1.7 output
-    P1SEL1 |= BIT6 | BIT7;                    // P1.6 and P1.7 options select
-
+    WDTCTL = WDTPW | WDTHOLD;
+    P1DIR |= BIT6 | BIT7;
+    P1SEL1 |= BIT6 | BIT7;
     PM5CTL0 &= ~LOCKLPM5;
-
-    TB0CCR0 = 8192-1;                          // PWM Period
-    TB0CCTL1 = OUTMOD_7;                      // CCR1 reset/set
-    TB0CCR1 = 1638.4;                            // CCR1 PWM duty cycle
-    TB0CTL = TBSSEL__ACLK | MC__UP | TBCLR;  // ACLK, up mode, clear TBR
+    TB0CCR0 = 8192-1;
+    TB0CCTL1 = OUTMOD_7;
+    TB0CCR1 = 1638.4;
+    TB0CTL = TBSSEL__ACLK | MC__UP | TBCLR;
 
     while(1)
     {
