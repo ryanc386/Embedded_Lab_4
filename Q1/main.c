c8cf9d2a0e63cc4f0e88b071a82bba7bb1aaaa36
ryanc386 <98830588+ryanc386@users.noreply.github.com>
2022-11-17 21:36:18 -0500

    Update main.c

--- Q1/main.c
+++ Q1/main.c
@@ -1,7 +1,7 @@
 #include <msp430.h>
 int main(void)
 {
-    WDTCTL = WDT_ADLY_250;     // WDT 1000ms, SMCLK, interval timer
+    WDTCTL = WDT_ADLY_250;
     P6OUT &= ~BIT6;
     P6DIR |= BIT6;
     PM5CTL0 &= ~LOCKLPM5;
